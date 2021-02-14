eb408de43bcbda8c375e7189820fbbba0389e588
David Edwards <commandertechnologysytems@gmail.com>
2021-02-13 19:51:57 -0600

    initialcommit

---
+++ GC Licensing Exam/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  GC Licensing Exam
+//
+//  Created by MacBook Pro on 2/13/21.
+//
+
+import Foundation
+
+import UIKit
+struct Question {
+  let question: String
+  let answer: String
+}
+let chapterOne: [Question] = [Question(question: "1. Detached one- and two-familiy dwellings must comply with: ", answer: "International Residential Code"), Question(question: "The purpose of the IBC code is to make sure the minimum requirements to protect public health, safety and general _______ are met", answer: "Welfare"), Question(question:"3. The ICC Electrical Code applies to the installation of: ", answer: "This is the third answer."), Question(question:"4. Installation and repair of a water or sewage system must be done in compliance with this code: ", answer:"International Plumbing Code"), Question(question:"5. International Property Maintenance Code applies to:", answer: "Equipment and Facilities")]
